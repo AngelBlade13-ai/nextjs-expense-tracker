6ed4693efede4621abae4d6afb9160c75b4a52ac
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-03-12 13:35:19 -0600

    Modals

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 "use client";
 
+import { useState } from "react";
+
 import { currencyFormatter } from "@/lib/utils";
 
 import ExpenseCategoryItem from "@/Components/ExpenseCategoryItem";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import Modal from "@/Components/Modal";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -43,54 +46,71 @@ const DUMMY_DATA = [
 ];
 
 export default function Home() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
   return (
-    <main className="container max-w-2xl px-6 py-6 mx-auto">
-      <section className="py-3">
-        <small className="text-gray-400 text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-        <section className=" flex items-center gap-2 py-3 ">
-          <button className="btn btn-primary">+ Expenses</button>
-          <button className="btn btn-primary-outline">+ Income</button>
+    <>
+      {/* Modal */}
+
+      <Modal show={modalIsOpen} onClose={setModalIsOpen}>
+        <h3>Hello World/</h3>
+      </Modal>
+
+      <main className="container max-w-2xl px-6 py-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+          <section className=" flex items-center gap-2 py-3 ">
+            <button
+              onClick={() => {
+                setModalIsOpen(true);
+              }}
+              className="btn btn-primary"
+            >
+              + Expenses
+            </button>
+            <button className="btn btn-primary-outline">+ Income</button>
+          </section>
         </section>
-      </section>
 
-      {/* Expenses */}
-      <section className=" py-6 ">
-        <h3 className=" text-2xl">My Expenses</h3>
-        <div className=" flex flex-col gap-4 mt-6 ">
-          {DUMMY_DATA.map((expense) => {
-            return (
-              <ExpenseCategoryItem
-                key={expense.id}
-                color={expense.color}
-                title={expense.title}
-                total={expense.total}
-              />
-            );
-          })}
-        </div>
-      </section>
+        {/* Expenses */}
+        <section className=" py-6 ">
+          <h3 className=" text-2xl">My Expenses</h3>
+          <div className=" flex flex-col gap-4 mt-6 ">
+            {DUMMY_DATA.map((expense) => {
+              return (
+                <ExpenseCategoryItem
+                  key={expense.id}
+                  color={expense.color}
+                  title={expense.title}
+                  total={expense.total}
+                />
+              );
+            })}
+          </div>
+        </section>
 
-      {/* Chart Section */}
-      <section className=" py-6 ">
-        <h3 className="text-2xl">Stats</h3>
-        <div className="w-1/2 mx-auto">
-          <Doughnut
-            data={{
-              labels: DUMMY_DATA.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Expenses",
-                  data: DUMMY_DATA.map((expense) => expense.total),
-                  backgroundColor: DUMMY_DATA.map((expense) => expense.color),
-                  borderColor: ["#18181b"],
-                  borderWidth: 5,
-                },
-              ],
-            }}
-          />
-        </div>
-      </section>
-    </main>
+        {/* Chart Section */}
+        <section className=" py-6 ">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: DUMMY_DATA.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: DUMMY_DATA.map((expense) => expense.total),
+                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
